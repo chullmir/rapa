4e380e0da854d62827ae15c655d0b47b180df51b
chullmir <paulochullmir@gmail.com>
2020-02-11 11:51:48 -0300

    primer commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.template')
+@section('title','Bienvenido')
+@section('content')
+Hola que tal
+@endsection
---
+++ resources/views/layouts/template.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>@yield('title')</title>
+</head>
+<body>
+	<header style="background: red">
+		<a href="#">Logo</a>
+	</header>
+	@yield('content')
+</body>
+</html>
